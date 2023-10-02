94717dee938491b546436ee1020a0d8e2b4f1272
GABI <gabriellimrqs1998@gmail.com>
2023-10-02 16:19:41 -0300

    radioButton

--- src/screens/CreateAds.tsx
+++ src/screens/CreateAds.tsx
@@ -1,24 +1,124 @@
-import React from "react";
-import { HStack, ScrollView, VStack, Text } from "native-base";
-import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { HStack, ScrollView, VStack, Text, useToast, Icon, Stack, TextArea } from "native-base";
+import { TouchableOpacity, View } from "react-native";
+
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { FileInfo } from "expo-file-system";
+import { ImagePickerSuccessResult } from "expo-image-picker";
 
 import { AntDesign } from '@expo/vector-icons'; 
 import { useNavigation } from "@react-navigation/native";
-import { AppNavigatorRoutesProp } from "../routes/app.routes";
 
+import { AppNavigatorRoutesProp } from "../routes/app.routes";
+import { AppError } from "../utils/AppError";
+import { Input } from "../components/Input";
+import { api } from "../services/api";
+
+
+type FormDataPropss = {
+    product_id: string;
+    avatar: string;
+}
 
 
 export function CreateAds() {
 
 const navigation = useNavigation<AppNavigatorRoutesProp>();
+const [avatar, setAvatar] = useState<ImagePickerSuccessResult>({} as ImagePickerSuccessResult)
+const [photoIsLoading, setPhotoIsLoading] = useState(false)
+//const [productPhoto, setProductPhoto] = useState("");
+
+const toast = useToast();
 
 function goBackToHome()  {
     navigation.goBack()
 }
 
+async function handleProductPhoto(){
+       
+try {
+        setPhotoIsLoading(true)
+
+        const photoSelected = await ImagePicker.launchImageLibraryAsync({
+             mediaTypes: ImagePicker.MediaTypeOptions.Images, 
+             quality: 1,
+             aspect: [4, 4],
+             allowsEditing:true
+        })
+
+        if(photoSelected.canceled){ 
+             return;
+        }
+
+        if(photoSelected.assets[0].uri) {
+            //setProductPhoto(photoSelected.assets[0].uri)
+            const photoInfo:FileInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+             
+            if(photoInfo.exists && !photoInfo.isDirectory) { 
+                 if(photoInfo.size && (photoInfo.size /1024 / 1024) > 5)
+                 return  toast.show({
+                     title: "Por favor escolha uma foto menor que 5MB.",
+                     placement: "top",
+                     bgColor: "red.600"
+                })
+            }
+        }
+
+         setAvatar(photoSelected);
+
+    } catch (error) {
+         
+        const isAppError = error instanceof AppError;
+        setPhotoIsLoading(false)
+        const title = isAppError ? error.message : "Não foi possível carregar a foto"
+
+        toast.show({
+             title,
+             placement: "top",
+        })    
+    }
+
+
+    async function showProductPhoto() {
+
+        try {
+
+        const fileExtension = avatar.assets[0].uri.split('.').pop();
+
+        const photoFile = {
+            name:`${fileExtension}`.toLocaleLowerCase(),
+            uri: avatar.assets[0].uri,
+            type: `${avatar.assets[0].uri}/${fileExtension}`
+        } as any;
+    
+        const formData = new FormData()
+    
+        formData.append("avatar", photoFile)
+    
+        const headers = {
+            "Content-type": "multipart/form-data"
+        }
+
+        const response = await api.post("/products/images", formData, {headers})
+        console.log(response.data)
+        
+
+
+        } catch (error) {
+            
+        }
+    }
+
+
+
+
+
+}
+
 return(
-    <ScrollView>
-        <VStack paddingTop={10} pl={8}>
+    <ScrollView paddingTop={10} pl={8}>
+        <VStack>
             <HStack>
                 <TouchableOpacity onPress={goBackToHome}>
                     <AntDesign name="arrowleft" size={26} color="gray.100" />
@@ -27,13 +127,37 @@ return(
                     Criar anúncio
                 </Text>
             </HStack>
-            <Text mt={6} fontFamily="heading" color={"gray.100"} fontSize={16}>Imagens</Text>
+            <Text mt={6} fontFamily="heading" color="gray.100" fontSize={16}>Imagens</Text>
             <Text mt={2} fontFamily="body" fontSize={14} color="gray.200">
                 Escolha até 3 imagens para mostrar o quanto seu 
                 produto é incrível!
             </Text>
-            
         </VStack>
+        <TouchableOpacity onPress={handleProductPhoto}>
+           <Stack backgroundColor="gray.500" borderRadius={6} mt={4} w={100} h={100} alignItems="center" justifyContent="center">
+                <AntDesign 
+                name="plus" 
+                size={26} 
+                color="gray"
+                /> 
+            </Stack>
+        </TouchableOpacity>
+        <Text mt={8} fontFamily="heading" color="gray.100" fontSize={16}>Sobre o Produto</Text>
+        
+        <Input w={325} mt={2} placeholder="Título do anúncio"/>
+
+        <TextArea autoCompleteType={undefined} 
+        backgroundColor="gray.700" 
+        borderRadius={6} 
+        borderColor="gray.700"
+        placeholder="Descrição do produto"
+        fontSize={16}
+        placeholderTextColor="gray.400"
+        mr={8}
+        />
+        
+      
+
     </ScrollView>
 )}
 
